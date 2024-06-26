ae73c2a7f3b9a13ed96a087ab4f95ce1ea75fd72
Nemanja Stjepanović <nemanjastjepanovic222@gmail.com>
2024-06-26 16:58:42 +0200

    Added footer

--- netflix-frontend/src/app/app.component.ts
+++ netflix-frontend/src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component,OnInit } from '@angular/core';
 import {NavigationEnd, Router, RouterOutlet} from '@angular/router';
 import {NavbarComponent} from "./features/home/components/navbar/navbar.component";
 import {NgIf} from "@angular/common";
+import {FooterComponent} from "./features/home/components/footer/footer.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,NavbarComponent,NgIf],
+  imports: [RouterOutlet, NavbarComponent, NgIf, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
