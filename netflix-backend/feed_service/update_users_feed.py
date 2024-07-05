edf868932895a4b1bc4ccf01d9cf88d4fe034ab0
Stefan Djurica <104830728+StefanDjuricaSV35-2021@users.noreply.github.com>
2024-07-05 14:22:38 +0200

    Refactored Update Feed

--- netflix-backend/feed_service/update_users_feed.py
+++ netflix-backend/feed_service/update_users_feed.py
@@ -27,6 +27,7 @@ def exponential_approach(date):
     result = result.quantize(Decimal('0.001'), rounding=ROUND_DOWN)
     return result
 
+
 def update_users_feed(event, context):
     try:
         body = json.loads(event['body'])
@@ -35,142 +36,7 @@ def update_users_feed(event, context):
         if not username:
             raise ValueError("Username is required")
 
-        genres_dict = {}
-        actors_dict = {}
-        directors_dict = {}
-
-        reviews = review_table.scan(
-            FilterExpression=Attr('username').eq(username)
-        )
-
-        if 'Items' in reviews:
-            for item in reviews['Items']:
-                if 'movie_id' in item:
-                    movie_id = item['movie_id']
-
-                    value = item.get('value', 1)
-                    value -= 2
-                    value = value / 2
-
-                    response = movies_table.query(
-                        KeyConditionExpression=boto3.dynamodb.conditions.Key('movie_id').eq(movie_id)
-                    )
-
-                    movie_items = response.get('Items', [])
-                    for movie_item in movie_items:
-                        genres = movie_item.get('genres', [])
-                        actors = movie_item.get('actors', [])
-                        directors = movie_item.get('directors', [])
-
-                        for genre in genres:
-                            if genre not in genres_dict:
-                                genres_dict[genre] = 0
-                            genres_dict[genre] += value
-
-                        for actor in actors:
-                            if actor not in actors_dict:
-                                actors_dict[actor] = 0
-                            actors_dict[actor] += value
-
-                        for director in directors:
-                            if director not in directors_dict:
-                                directors_dict[director] = 0
-                            directors_dict[director] += value
-
-        subscriptions = subscription_table.scan(
-            FilterExpression=Attr('username').eq(username)
-        )
-
-        if 'Items' in subscriptions:
-            for item in subscriptions['Items']:
-                if 'type' in item and 'value' in item:
-                    type = item['type']
-                    value = item['value']
-
-                    if type == 'actor':
-                        if value not in actors_dict:
-                            actors_dict[value] = 0
-                        actors_dict[value] += 1
-
-                    elif type == 'genre':
-                        if value not in genres_dict:
-                            genres_dict[value] = 0
-                        genres_dict[value] += 1
-
-                    elif type == 'director':
-                        if value not in directors_dict:
-                            directors_dict[value] = 0
-                        directors_dict[value] += 1
-
-        downloads = download_history_table.scan(
-            FilterExpression=Attr('username').eq(username)
-        )
-
-        if 'Items' in downloads:
-            for item in downloads['Items']:
-                if 'movie_id' in item and 'timestamp' in item:
-                    movie_id = item['movie_id']
-                    timestamp = item['timestamp']
-
-                    download_datetime = datetime.datetime.fromisoformat(timestamp)
-                    value = exponential_approach(download_datetime)
-                    print(value)
-                    response = movies_table.query(
-                        KeyConditionExpression=boto3.dynamodb.conditions.Key('movie_id').eq(movie_id)
-                    )
-                    movie_items = response.get('Items', [])
-                    for movie_item in movie_items:
-                        genres = movie_item.get('genres', [])
-                        actors = movie_item.get('actors', [])
-                        directors = movie_item.get('directors', [])
-
-                        for genre in genres:
-                            if genre not in genres_dict:
-                                genres_dict[genre] = 0
-                            genres_dict[genre] += value
-
-                        for actor in actors:
-                            if actor not in actors_dict:
-                                actors_dict[actor] = 0
-                            actors_dict[actor] += value
-
-                        for director in directors:
-                            if director not in directors_dict:
-                                directors_dict[director] = 0
-                            directors_dict[director] += value
-
-        movie_weights = {}
-
-        response = movies_table.scan()
-        movies = response['Items']
-
-        for movie in movies:
-            movie_id = movie['movie_id']
-            genres = movie.get('genres', [])
-            actors = movie.get('actors', [])
-            directors = movie.get('directors', [])
-
-            weight = 0
-
-            for genre in genres:
-                if genre in genres_dict:
-                    weight += genres_dict[genre]
-
-            for actor in actors:
-                if actor in actors_dict:
-                    weight += actors_dict[actor]
-
-            for director in directors:
-                if director in directors_dict:
-                    weight += directors_dict[director]
-
-            movie_weights[movie_id] = weight
-        print(genres_dict)
-        print(directors_dict)
-        print(actors_dict)
-        print(movie_weights)
-        sorted_movies = sorted(movie_weights.items(), key=lambda x: x[1], reverse=True)
-        top_movie_ids = [movie_id for movie_id, weight in sorted_movies[:9]]
+        top_movie_ids = get_top_movies(username)
 
         existing_feed = feed_table.get_item(Key={'username': username})
 
@@ -221,3 +87,142 @@ def update_users_feed(event, context):
             },
             'body': json.dumps({'error': str(e)})
         }
+
+def get_top_movies(username):
+    genres_dict = {}
+    actors_dict = {}
+    directors_dict = {}
+
+    reviews = review_table.scan(
+        FilterExpression=Attr('username').eq(username)
+    )
+
+    if 'Items' in reviews:
+        for item in reviews['Items']:
+            if 'movie_id' in item:
+                movie_id = item['movie_id']
+
+                value = item.get('value', 1)
+                value -= 2
+                value = value / 2
+
+                response = movies_table.query(
+                    KeyConditionExpression=boto3.dynamodb.conditions.Key('movie_id').eq(movie_id)
+                )
+
+                movie_items = response.get('Items', [])
+                for movie_item in movie_items:
+                    genres = movie_item.get('genres', [])
+                    actors = movie_item.get('actors', [])
+                    directors = movie_item.get('directors', [])
+
+                    for genre in genres:
+                        if genre not in genres_dict:
+                            genres_dict[genre] = 0
+                        genres_dict[genre] += value
+
+                    for actor in actors:
+                        if actor not in actors_dict:
+                            actors_dict[actor] = 0
+                        actors_dict[actor] += value
+
+                    for director in directors:
+                        if director not in directors_dict:
+                            directors_dict[director] = 0
+                        directors_dict[director] += value
+
+    subscriptions = subscription_table.scan(
+        FilterExpression=Attr('username').eq(username)
+    )
+
+    if 'Items' in subscriptions:
+        for item in subscriptions['Items']:
+            if 'type' in item and 'value' in item:
+                type = item['type']
+                value = item['value']
+
+                if type == 'actor':
+                    if value not in actors_dict:
+                        actors_dict[value] = 0
+                    actors_dict[value] += 1
+
+                elif type == 'genre':
+                    if value not in genres_dict:
+                        genres_dict[value] = 0
+                    genres_dict[value] += 1
+
+                elif type == 'director':
+                    if value not in directors_dict:
+                        directors_dict[value] = 0
+                    directors_dict[value] += 1
+
+    downloads = download_history_table.scan(
+        FilterExpression=Attr('username').eq(username)
+    )
+
+    if 'Items' in downloads:
+        for item in downloads['Items']:
+            if 'movie_id' in item and 'timestamp' in item:
+                movie_id = item['movie_id']
+                timestamp = item['timestamp']
+
+                download_datetime = datetime.datetime.fromisoformat(timestamp)
+                value = exponential_approach(download_datetime)
+                print(value)
+                response = movies_table.query(
+                    KeyConditionExpression=boto3.dynamodb.conditions.Key('movie_id').eq(movie_id)
+                )
+                movie_items = response.get('Items', [])
+                for movie_item in movie_items:
+                    genres = movie_item.get('genres', [])
+                    actors = movie_item.get('actors', [])
+                    directors = movie_item.get('directors', [])
+
+                    for genre in genres:
+                        if genre not in genres_dict:
+                            genres_dict[genre] = 0
+                        genres_dict[genre] += value
+
+                    for actor in actors:
+                        if actor not in actors_dict:
+                            actors_dict[actor] = 0
+                        actors_dict[actor] += value
+
+                    for director in directors:
+                        if director not in directors_dict:
+                            directors_dict[director] = 0
+                        directors_dict[director] += value
+
+    movie_weights = {}
+
+    response = movies_table.scan()
+    movies = response['Items']
+
+    for movie in movies:
+        movie_id = movie['movie_id']
+        genres = movie.get('genres', [])
+        actors = movie.get('actors', [])
+        directors = movie.get('directors', [])
+
+        weight = 0
+
+        for genre in genres:
+            if genre in genres_dict:
+                weight += genres_dict[genre]
+
+        for actor in actors:
+            if actor in actors_dict:
+                weight += actors_dict[actor]
+
+        for director in directors:
+            if director in directors_dict:
+                weight += directors_dict[director]
+
+        movie_weights[movie_id] = weight
+    print(genres_dict)
+    print(directors_dict)
+    print(actors_dict)
+    print(movie_weights)
+    sorted_movies = sorted(movie_weights.items(), key=lambda x: x[1], reverse=True)
+    top_movie_ids = [movie_id for movie_id, weight in sorted_movies[:9]]
+    return top_movie_ids
