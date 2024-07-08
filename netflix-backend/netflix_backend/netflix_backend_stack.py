df5a530668e1d1ef8ea7c78e124e4656f81cf1c9
Borislav Čelar <bora.celar@gmail.com>
2024-07-08 21:57:15 +0200

    cors update

--- netflix-backend/netflix_backend/netflix_backend_stack.py
+++ netflix-backend/netflix_backend/netflix_backend_stack.py
@@ -305,7 +305,7 @@ class NetflixBackendStack(Stack):
                                 rest_api_name="netflix-api",
                                 endpoint_types=[apigateway.EndpointType.REGIONAL],
                                 default_cors_preflight_options=apigateway.CorsOptions(
-                                    allow_origins=["*"],
+                                    allow_origins=["http://localhost:4200"],
                                     allow_methods=["PUT","POST","DELETE","GET","HEAD"],
                                     allow_headers=["*"]
                                 ))
